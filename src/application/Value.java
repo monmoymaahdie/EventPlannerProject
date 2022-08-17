5afe19a24e22d6716010b25f1b64ab8a27cd4dc2
Monmoy <Monmoy@10.12.185.41>
2022-08-16 21:51:42 -0600

    .

--- src/application/Value.java
+++ src/application/Value.java
@@ -1,5 +1,57 @@
+
 package application;
 
 public class Value {
+	private double amount;
+	private int min;
+	private int max;
+	
+	public Value (double amount, int min, int max){
+		this.amount = amount;
+		this.min = min;
+		this.max = max;
+	}
+	
 
-}
+
+	public String setValue(String valueAsString) {
+		String errorMessage = " ";
+		boolean validEntry = true;
+		if (!valueAsString.isEmpty()) {
+			for (char c: valueAsString.toCharArray()) {
+				if (!Character.isDigit(c)) {
+					if (!valueAsString.matches("\\d+\\.\\d+")) {
+	    				validEntry = false;
+	    				errorMessage = String.format("Do not use %c. Make sure to enter a number.", c);
+	    			}
+				}
+			}
+		}
+			else {
+				validEntry = false;
+				errorMessage ="Make sure to enter a valid number.";
+			}
+		
+		if(validEntry) {
+			this.amount = Double.parseDouble(valueAsString);
+		}
+		
+		if (this.amount < this.min || this.amount > this.max){
+			errorMessage = String.format("Amount should be between 1 and %d", this.max);
+		}
+		return errorMessage;
+		}
+	
+	public double getAmount() {
+		return this.amount;
+	}
+
+	public int getMax() {
+		return this.max;
+	}
+	
+	public int getMin() {
+		return this.min;
+	}
+	
+	}
